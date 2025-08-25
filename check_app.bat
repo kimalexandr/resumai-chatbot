939587798f3b9f74bf6757640629dd8eba51912c
kimalexandr <kimalexandr@gmail.com>
2025-08-25 14:12:44 +0300

    Очистка проекта: удаление лишних bat-файлов, оставлены только основные

--- check_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Checking if ResumAI Chatbot is running...
-netstat -an | findstr :5000
-if %errorlevel% equ 0 (
-    echo App is running on port 5000
-) else (
-    echo App is NOT running on port 5000
-)
-pause
